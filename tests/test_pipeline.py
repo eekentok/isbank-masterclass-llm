1c0885d64f007d8c17301e2b29dc78e5e335c448
Ercüment Ekentok <ercument.ekentok@ozu.edu.tr>
2025-07-02 02:59:11 +0300

    Create test_pipeline.py

---
+++ tests/test_pipeline.py
@@ -0,0 +1,22 @@
+# tests/test_pipeline.py
+
+"""
+test_pipeline.py
+
+Amaç:
+- Basit test kontrolleri
+"""
+
+def test_no_empty_rows():
+    """
+    TODO:
+    - Cleaned CSV'de boş satır var mı kontrol et
+    """
+    pass
+
+def test_chunk_length():
+    """
+    TODO:
+    - Chunk uzunluğu limit kontrolü
+    """
+    pass
